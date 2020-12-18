20a7ad1d967dc178d812d591e825a7066243abf7
Marmik Upadhyay <marmikupadhyay.mu@gmail.com>
2020-12-18 11:38:34 +0530

    feat : Add Custom _id  to reduce to 6 char.

--- src/api/controllers/QueueController.js
+++ src/api/controllers/QueueController.js
@@ -3,6 +3,72 @@ const Queue = require('../../models/Queue');
 const User = require('../../models/User');
 const ObjectId = require('mongoose').Types.ObjectId;
 
+const getNewId = () => {
+	const base62 = {
+		charset: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(
+			''
+		),
+		encode: (integer) => {
+			if (integer === 0) {
+				return 0;
+			}
+			let s = [];
+			while (integer > 0) {
+				s = [base62.charset[integer % 62], ...s];
+				integer = Math.floor(integer / 62);
+			}
+			return s.join('');
+		},
+		decode: (chars) =>
+			chars
+				.split('')
+				.reverse()
+				.reduce(
+					(prev, curr, i) =>
+						prev + base62.charset.indexOf(curr) * 62 ** i,
+					0
+				),
+	};
+
+	let newId = base62.encode(
+		Math.floor(Math.random() * 10000000000 + 1000000000)
+	);
+	return newId;
+};
+
+const createRecursion = (req, res, next, newId) => {
+	Queue.findOne({ _id: newId }, (err, queue) => {
+		if (err || queue) {
+			createRecursion(req, res, next, getNewId());
+		} else {
+			var newQueue;
+			newQueue = new Queue({ _id: newId });
+			newQueue.save((err) => {
+				if (err) {
+					console.log(err);
+				}
+			});
+			User.update(
+				{ _id: req.params.user_id },
+				{ $push: { queue: newQueue._id } }
+			)
+				.then((user) => {
+					res.status(200).json({
+						message: 'Queue Created Succesfully',
+						data: newQueue,
+					});
+				})
+				.catch((err) => {
+					console.log(err);
+					res.status(500).json({
+						message: 'Internal Server Error',
+						error: err,
+					});
+				});
+		}
+	});
+};
+
 const QueueController = {
 	getQueueFront(req, res, next) {
 		Queue.findOne({ _id: req.params.queue_id })
@@ -147,30 +213,8 @@ const QueueController = {
 						data: {},
 					});
 				} else {
-					var newQueue;
-					newQueue = new Queue();
-					newQueue.save((err) => {
-						if (err) {
-							console.log(err);
-						}
-					});
-					User.update(
-						{ _id: req.params.user_id },
-						{ $push: { queue: newQueue._id } }
-					)
-						.then((user) => {
-							res.status(200).json({
-								message: 'Queue Created Succesfully',
-								data: newQueue,
-							});
-						})
-						.catch((err) => {
-							console.log(err);
-							res.status(500).json({
-								message: 'Internal Server Error',
-								error: err,
-							});
-						});
+					let newId = getNewId();
+					createRecursion(req, res, next, newId);
 				}
 			})
 			.catch((err) => {
--- src/models/Queue.js
+++ src/models/Queue.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const QueueSchema = new mongoose.Schema({
+	_id: { type: String },
 	current: [mongoose.Types.ObjectId],
 });
 
